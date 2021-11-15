e0ee545311cc754d117b6e9fa5419746fbe13ed2
CosmoKotik <47870598+CosmoKotik@users.noreply.github.com>
2021-11-14 23:17:48 -0500

    Added ALCompiler and added config file

--- authlib-1.5.25/BuildAuthLib.bat
+++ authlib-1.5.25/BuildAuthLib.bat
@@ -1,19 +1,30 @@
 @echo off
+echo Custom AuthLib by:CosmoKotik
+
+:configloader
+rename config.ini config.bat
+call config.bat
+rename config.bat config.ini
+
+:AuthLib recompiller
+java -jar ALCompiler.jar src %AuthServerIP%
+
+:Building/Compiling AuthLib
 ::javac -cp libraries\log4j-core-2.8.1.jar,libraries\gson-2.8.0.jar src\com\mojang\util\*.java
-:: javac -d build\ src\com\mojang\util\*.java
+:javac -d build\ src\com\mojang
 :: javac -d build\ src\com\mojang\authlib\*.java
 :: javac -d build\ src\com\mojang\authlib\exceptions\*.java
-:: javac -d build\ src\com\mojang\authlib\legacy\*.java
+:javac -cp .:libraries/commons-lang3-3.5.jar:libraries/log4j-core-2.8.1.jar -d build\ src\com\mojang\authlib\legacy\*.java
 :: javac -d build\ src\com\mojang\authlib\minecraft\*.java
 :: javac -d build\ src\com\mojang\authlib\properties\*.java
-:: javac -d build\ src\com\mojang\authlib\yggdrasil\*.java
+:javac -d build\ src\com\mojang\authlib\yggdrasil\*.java
 :: javac -d build\ src\com\mojang\authlib\yggdrasil\request\*.java
 :: javac -d build\ src\com\mojang\authlib\yggdrasil\response\*.java
 
 ::javac -cp libraries\* src\*.java
 
+:Building .jar file
 echo Starting building .jar file
-::jar cvfm out\authlib-1.5.25.jar manifest.txt -C build .
 jar cvfM out\authlib-1.5.25.jar -C build .
 
 if errorlevel 1 (
